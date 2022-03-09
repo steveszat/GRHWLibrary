6ae339c3bb60f5bda2e324575a20f05c06d2f667
steveszat <steve.szatkowski@gmail.com>
2022-03-09 17:58:53 -0600

    Add files via upload

---
+++ SomeFileReader.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRHWLibrary
+{
+    public class SomeFileReader : ISomeFileReader
+    {
+        private string _filePath;
+
+        public SomeFileReader(string filePath)
+        {
+            _filePath = filePath;
+
+        }
+
+        public string[] OpenFile()
+        {
+            string[] result;
+            try
+            {
+                result = File.ReadAllLines(_filePath);
+            }
+            // catch and log exception and return generic error message
+            catch (ArgumentException)
+            {
+                throw;
+            }
+            catch (PathTooLongException)
+            {
+                throw;
+            }
+            catch (DirectoryNotFoundException)
+            {
+                throw;
+            }
+            catch (FileNotFoundException)
+            {
+                throw;
+            }
+            catch (IOException)
+            {
+                throw;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                throw;
+            }
+            catch (NotSupportedException)
+            {
+                throw;
+            }
+            catch (SecurityException)
+            {
+                throw;
+            }
+            return result;
+        }
+    }
+}
